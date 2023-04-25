10f1c8e1f4ec3640db4a6d83c1df9eb527e8d357
Carlos Silva <88803987+carloosf@users.noreply.github.com>
2023-04-25 14:09:24 -0300

    move

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -1,13 +1,13 @@
-import './App.css';
+import './styles/App.css';
 import AudioDownloader from './components/AudioDownloader/AudioDownloader';
 
 function App() {
   return (
-    <>
+    <div className='App'>
     <header></header>
       <AudioDownloader />
       <footer></footer>
-    </>
+    </div>
   );
 }
 
--- Front/src/components/AudioDownloader/AudioDownloader.jsx
+++ Front/src/components/AudioDownloader/AudioDownloader.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import '../../styles/AudioDownloader.css'
 
 function AudioDownloader() {
     const [url, setUrl] = useState('');
@@ -8,16 +9,27 @@ function AudioDownloader() {
         try {
             axios.get(`http://localhost:3001/?url=${url}`)
             console.log(url);
+
         } catch (error) {
             console.error(error);
+        }
+        finally {
+            setUrl('')
         }
     }
 
     return (
         <div>
             <h1>YouTube Audio Downloader</h1>
-            <input id="url-input" type="text" value={url} onChange={e => setUrl(e.target.value)} />
-            <button onClick={handleDownload}>Enviar</button>
+            <form className='formUrl'>
+                <input
+                    type="text"
+                    value={url}
+                    onChange={e => setUrl(e.target.value)}
+                    placeholder='Insira o link do video...'
+                />
+                <button onClick={handleDownload}>Enviar</button>
+            </form>
         </div>
     );
 }
